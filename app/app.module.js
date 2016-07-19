0b909c4582929e64e62196a31145af83faa4e2d0
schultzRr <ZFja5Hdb>
2016-07-19 14:39:07 -0500

    Se agregó autenticación y ruteo de vistas a la aplicación

--- app/app.module.js
+++ app/app.module.js
@@ -7,9 +7,22 @@ angular.module('omtApp', [
     'ngMaterial',
     'md-steppers',
     'LocalStorageModule',
+    'intro',
+    'login',
     'navigation',
     'customizer',
     'collectionTiles',
     'collectionGrids',
     'tileCustomizer'
 ]);
+
+angular.module('omtApp')
+    .constant('AUTH_API_URL_BASE', 'http://198.61.202.55:8081')
+    .constant('API_URL_BASE', 'http://198.61.202.55:8081');
+
+angular.module('omtApp')
+    .run(['$rootScope', '$route', 'routingService', function($rootScope, $route, routingService) {
+        $rootScope.$on('$routeChangeSuccess', function() {
+            routingService.setView($route.current.view);
+        });
+    }]);
--- app/components/navigation/navigation.component.js
+++ app/components/navigation/navigation.component.js
@@ -1,13 +1,43 @@
 (function(angular) {
     'use strict';
 
-    function navigationController() {
+    function navigationController($location, navigationService, sessionService, routingService) {
 
         /**
          *
          * @type {Object}
          */
         var ctrl = this;
+
+        /**
+         *
+         * @returns {*}
+         */
+        ctrl.isAuthenticated = function() {
+            return sessionService.isAuthenticated();
+        };
+
+        /**
+         * Closes user's session
+         */
+        ctrl.logout = function() {
+            navigationService.logout()
+                .then(function(data) {
+                    $location.path('/login');
+                }, function(error) {
+                    if(error && error.errors){
+                        console.log(error.errors[0].title);
+                    }
+                });
+        };
+
+        ctrl.getView = function() {
+            return routingService.getView();
+        };
+
+        ctrl.goTo = function(path) {
+            $location.path(path);
+        };
     }
 
     angular
