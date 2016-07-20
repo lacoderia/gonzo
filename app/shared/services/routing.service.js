88c34e4b675108e8d729adb28e58ea0d6d9c79bb
schultzRr <rosas_schultz@hotmail.com>
2016-07-19 22:47:37 -0500

    Se clonaron las formas para recuperar y resetear la contraseña del usuario

--- app/shared/services/routing.service.js
+++ app/shared/services/routing.service.js
@@ -13,9 +13,25 @@
             _view = view;
         };
 
+        var getParam = function(param) {
+            var vars = {};
+            window.location.href.replace( location.hash, '' ).replace(
+                /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+                function( m, key, value ) { // callback
+                    vars[key] = value !== undefined ? value : '';
+                }
+            );
+
+            if ( param ) {
+                return vars[param] ? vars[param] : null;
+            }
+            return vars;
+        };
+
         var service = {
             getView: getView,
-            setView: setView
+            setView: setView,
+            getParam: getParam
         };
 
         return service;
